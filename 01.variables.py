f5c2ae3c1cfdce07fe28bf8dac98b1e76a19811c
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-23 18:20:53 -0500

    python practice

---
+++ 01.variables.py
@@ -0,0 +1,24 @@
+a = 5
+b = 2
+
+total = a + b
+diff = a - b
+mul = a * b
+div = a/b
+int_div = a // b
+mod_div = a % b
+power = a ** b
+
+print(total)
+print(diff)
+print(mul)
+print(div)
+print(int_div)
+print(mod_div)
+print(power)
+
+
+# Order of Operation. (DMAS or BODMAS)
+
+ans = 2 + 10 * 10 + 3
+print (ans)
---
+++ 02.variables.py
@@ -0,0 +1,9 @@
+# sample = 'a'
+# sample_str = "abc"
+# sample_str = """abc"""
+# sample_str = "Today's weather is nice"
+
+sample_str = """hi how are you"""
+# print(len(sample_str))
+print(sample_str.count(are))
+
