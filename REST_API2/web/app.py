e35c9dd964aae0bcc07ac4e989d751b2fc96074d
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-06-09 16:03:36 -0500

    python changes

--- REST_API2/web/app.py
+++ REST_API2/web/app.py
@@ -1,8 +1,3 @@
-# - Registration of a user 0 tokens
-# - Each user gets 10 tokens
-# - Store a sentence on our database  for 1 token
-# - Retreive his stored sentence on out database for 1 token
-
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 import os
@@ -13,52 +8,9 @@ app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
-db = client.SentancesDatabase
+db = client.SentencesDatabase
 Users = db["Users"]
 
-class Register(Resource):
-    def post(self):
-        
-        
-        #step 1: is to get the posted data by user
-        postedData = request.get_json()
-
-        username = postedData["username"]
-        password = postedData["password"]
-
-        #hash(123xyz +salt) = jskshsklsahlksjhdljh
-        hashed_pw = bcrypt.hashpw(password.enclode('utf8'), bcrypt.gensalt())
-
-        # store username and pw into the SentanceDatabase
-        Users.insert_one({
-            "Username" : username,
-            "password" : hashpw,
-            "Sentence" : "",
-            "Tokens" :6
-        })
-
-        retJSON = {
-            "status" : 200,
-            "msg" : "you have successfully connected to API"
-        }   
-        return jsonify(retJSON)
-    
-    def verifyPW(username, password):
-        hashed_pw = Users.find({
-            "Username" : username
-        })[(0)["Password"]]
-
-        if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
-            return True
-        else:
-            return False
-
-    def verifyTokens(username):
-        tokens = Users.find({
-            "Username" : username
-        })[(0)["Tokens"]]
-        return tokens
-    
 class Register(Resource):
     def post(self):
         postedData = request.get_json()
@@ -73,7 +25,7 @@ class Register(Resource):
         })
         retJSON = {
             "status" : 200,
-            "msg" : "you have successfully connected to API"
+            "msg" : "You have successfully connected to API"
         }   
         return jsonify(retJSON)
 
@@ -82,14 +34,14 @@ class Store(Resource):
         postedData = request.get_json()
         username = postedData["username"]
         password = postedData["password"]
-        sentence = postedData["sentence"]  # Correct the key for the sentence
-        correct_pw = Register.verifyPW(username, password)
+        sentence = postedData["sentence"]
+        correct_pw = self.verifyPW(username, password)
         if not correct_pw:
             retJSON = {
-                "Status" : 302
+                "status" : 302
             }
             return jsonify(retJSON)
-        num_tokens = Register.verifyTokens(username)
+        num_tokens = self.verifyTokens(username)
         if num_tokens <= 0:
             retJSON = {
                 "status" : 301
@@ -109,14 +61,25 @@ class Store(Resource):
             "msg": "Sentence saved successfully"
         }
         return jsonify(retJSON)
-        
+    
+    @staticmethod
+    def verifyPW(username, password):
+        user = Users.find_one({"Username": username})
+        if user:
+            hashed_pw = user["password"]
+            if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
+                return True
+        return False
+
+    @staticmethod
+    def verifyTokens(username):
+        user = Users.find_one({"Username": username})
+        if user:
+            return user["Tokens"]
+        return 0
+
 api.add_resource(Register, "/register")
 api.add_resource(Store, "/store")
 
-@app.route('/')
-def gsp_world():
-    return "hi gsp how are you"
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
